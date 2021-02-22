c4ffeeb58d00bc6da6c75e13a90d8d1bceb7277c
Niranjan K <niranjank7477@gmail.com>
2021-02-22 11:52:40 +0530

    api creation

---
+++ backend/db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://127.0.0.1:27017/placement-experience-db',{
+    useNewUrlParser:true,
+    useCreateIndex:true
+})
+const PlaceExp = new Schema({
+    name : {
+        type:String
+    },
+    email :{
+        type:String
+    },
+    year :{
+        type:String
+    },
+    company : {
+        type:String
+    },
+    linkedinlink : {
+        type:String
+    },
+    experiencefile :{
+        data: Buffer, 
+        contentType: String
+    }
+})
+module.exports = mongoose.model('experienceschema',PlaceExp)
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const multer = require("multer");
+const uuidv4 = require("uuid").v4;
+const experienceschema = require('./db.js');
+const fs = require('fs');
+const path = require('path')
 const app = express();
 
 var corsOptions = {
@@ -14,11 +18,31 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads');
+  },
+  filename: (req, file, cb) => {
+   const newFilename = `${uuidv4()}${path.extname(file.originalname)}`;
+   cb(null, newFilename);
+  },
+});
+const upload = multer({ storage });
 
 app.get("/", (req, res) => {
   res.json({ message: "Hi." });
 });
-
+app.post('/add', upload.single('selectedFile'), (req, res) => {
+  const experienceschemauser = new experienceschema;
+  experienceschemauser.name = req.body.name;
+  experienceschemauser.linkedinlink = req.body.linkedinlink;
+  experienceschemauser.experiencefile.data = fs.readFileSync('./uploads/' + req.file.filename)
+  experienceschemauser.experiencefile.contentType = "document/pdf"
+  console.log(req.body);
+  console.log(req.file.filename);
+  experienceschemauser.save()
+  res.status(200).send("Inserted");
+});
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
